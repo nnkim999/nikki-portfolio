2a588e31a446fa6df086854797680cfea01c4559
nayeon.kim <nayeon.kim@ucalgary.ca>
2025-04-28 17:41:30 -0600

    added about me section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,20 +3,25 @@ import "../Styles/Home.css";
 import profileVideo from "../Media/profile.mp4";
 import linkedinicon from "../Media/linkedinicon.png";
 import emailinicon from "../Media/emailicon.png";
+import aboutVideo from "../Media/aboutvideo.mp4"
 
 function Home() {
-  const videoRef = useRef(null);
+  const profileVideoRef = useRef(null);
+  const aboutVideoRef = useRef(null);
 
   const handleLoadedData = () => {
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.3;
+    if (profileVideoRef.current) {
+      profileVideoRef.current.playbackRate = 0.3;
     }
   };
 
+
+
   return (
     <div className="main-section">
+      <div className="mt"></div>
       <div className="main-content">
-        <div className="first-left-side">
+        <div className="half-left-side">
           <h1>Hi, I’m Nikki Kim</h1>
           <h2>Computer Science Student</h2>
           <button className="cta-button">More about me</button>
@@ -37,15 +42,15 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="first-right-side">
-          <div className="profile-video-container">
+        <div className="half-right-side">
+          <div className="round-video-container">
             <video
-              ref={videoRef}
+              ref={profileVideoRef}
               autoPlay
               loop
               muted
               playsInline
-              className="profile-video"
+              className="round-video"
               onLoadedData={handleLoadedData}
               style={{
                 pointerEvents: 'none' // Disables all interactions
@@ -56,7 +61,43 @@ function Home() {
           </div>
         </div>
       </div>
+      <div className="mt"></div>
+      <div className="about-content">
+            <div className='half-left-side'>
+              <div className="round-video-container">
+                <video
+                  ref={aboutVideoRef}
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  className="round-video-lg"
+                  onLoadedData={handleLoadedData}
+                  style={{
+                    pointerEvents: 'none' // Disables all interactions
+                  }}
+                >
+                  <source src={aboutVideo} type="video/mp4" />
+                </video>
+            </div>
+            </div>
+            <div className='half-right-side'>
+              <div className='text-section-container'>
+                <h1>About me</h1>
+                <h2>Hi, I’m Nikki, a third-year Computer Science student at the University of Calgary with a passion for software and creative problem-solving. 
+                </h2>
+                <h2>
+                Through my internship, personal projects, and hackathons, I’ve built a strong foundation in technical development and teamwork.                 For extracurriculars, I’m active with WiCS (Women in Computer Science), Competitive Programming Club, and the Garden Club!
+                </h2>
+                <h2>
+                In my free time, I love reading books, running marathons, and gardening!
+                </h2>
+              </div>
+            </div>
+      </div>
     </div>
+
+
   );
 }
 
